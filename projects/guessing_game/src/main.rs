60349d9b9d83e1e3d04e773dc73dffbe6b5b444e
Rip Kahlon <ripkahlon@live.com>
2018-01-01 21:46:16 -0500

    Added rand crate

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,8 +1,16 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     //Sending an input, the ! indicates a macro instead of a method call.
     println!("Guess a number");
+
+    //Generate a secret random number
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
     println!("Please input your guess: ");
 
 
